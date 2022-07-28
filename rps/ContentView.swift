7e4b122496ff56886bc0d5acb7845f9e2d00b9e7
Ajay Darisi <dgvsak7@gmail.com>
2022-07-28 19:42:55 +0530

    Initial Commit

---
+++ rps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rps
+//
+//  Created by Ajay Darisi on 28/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rps/rpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rpsApp.swift
+//  rps
+//
+//  Created by Ajay Darisi on 28/07/22.
+//
+
+import SwiftUI
+
+@main
+struct rpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
